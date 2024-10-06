5bf3786fed085fc66df9c9e951bd3d305781044d
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-10-06 13:27:37 +0200

    main logic done

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,6 @@
 import Search from "./Search"
 import API_KEY from "../ApiKeys";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Weather() {
     const [search, setSearch] = useState('');
@@ -8,21 +8,43 @@ export default function Weather() {
     const [weatherData, setWeatherData] = useState(null);
 
     async function fetchData(param) {
+        setLoading(true);
         try {
             const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}/?key=${API_KEY} `)
             const data = await response.json();
+
+            if (data) {
+                setWeatherData(data)
+                setLoading(false)
+            }
             console.log(data, "data");
 
         } catch (e) {
+            setLoading(false)
             console.log(e);
 
         }
     }
-
     function handleSearch() {
         fetchData(search)
     }
 
+    function getCurrentDate() {
+        let date = new Date()
+        const options =
+        {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+
+        }
+        date = date.toLocaleDateString('en-US', options)
+        return date;
+    }
+
+    useEffect(() => {
+    }, [])
 
     return (
         <div>
@@ -30,6 +52,39 @@ export default function Weather() {
                 setSearch={setSearch}
                 handleSearch={handleSearch}
             />
+            {loading ?
+                <div className="loader">Loading...</div>
+                :
+                <div className="city-name">
+                    <h2>
+                        {weatherData?.resolvedAddress}
+                    </h2>
+                    <div className="date">
+                        <span> {getCurrentDate()} </span>
+                    </div>
+                    <div className="temperature">
+                        {/* temperatura */}
+                        {weatherData?.currentConditions.dew}
+                    </div>
+                    <p className="description">
+                        {weatherData?.description}
+                    </p>
+                    <div className="weather-info">
+                        <div>
+                            <div>
+                                <p className="wind"> {weatherData?.currentConditions.windspeed} </p>
+                                <p>Wind speed</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <p className="humidity"> {weatherData?.currentConditions.humidity}% </p>
+                                <p>Humidity</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
